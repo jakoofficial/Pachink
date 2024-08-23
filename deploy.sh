bac1da47d614368135b715be34fe77b2cb740dbf
hoxer <simonhoxer@gmail.com>
2024-08-23 10:53:32 +0200

    Added pulling to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+# GET latest version
+git pull
+
 # EXPORT client
 godot Pachink/project.godot --headless --export-release "Web" ../client-docker/export/index.html
 
 docker-compose build
-docker-compose up
+docker-compose up -d
