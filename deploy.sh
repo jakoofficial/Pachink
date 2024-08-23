67a97375537557bf319a9514ac82ccb2f2af1c09
hoxer <simonhoxer@gmail.com>
2024-08-23 12:02:55 +0200

    Changed export presets

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@
 git pull
 
 # EXPORT client
+cd Game
 godot Pachink/project.godot --headless --export-release "Web" ../client-docker/export/index.html
+cd ..
 
 docker-compose build
 docker-compose up -d
