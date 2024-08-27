4005d900966c7aae8c2c67bccd877cb3ecd509e9
Jako <jacobvoandersen@gmail.com>
2024-08-27 14:57:28 +0200

    Update the tileset and removed the pins from it. Added the level_button as a saved node. Started work on the level select menu

--- Game/Scripts/main_menu.gd
+++ Game/Scripts/main_menu.gd
@@ -2,6 +2,15 @@ extends Node2D
 
 @onready var settings: Control = $CanvasLayer/Settings
 @onready var fullscreen_box = $CanvasLayer/Settings/CenterContainer/VBoxContainer/HBoxContainer/CheckBox as CheckBox
+@onready var board_select = $CanvasLayer/BoardSelect/CenterContainer/HBoxContainer
+
+var board_button = preload("res://Nodes/level_button.tscn")
+
+func _ready():
+	var l1 = board_button.instantiate() as Button
+	l1.text = "HI!!!!"
+	board_select.add_child(l1)
+
 
 func _process(delta):
 	if Manager.windowMode == DisplayServer.WINDOW_MODE_FULLSCREEN:
