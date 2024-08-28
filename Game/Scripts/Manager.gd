1c98a4b3ea80955fecafabb4ab4439ff65bd13c7
Jako <jacobvoandersen@gmail.com>
2024-08-28 11:43:30 +0200

    Added a pause menu button on the screen. made it so the game over box comes up when last ball have been used.

--- Game/Scripts/Manager.gd
+++ Game/Scripts/Manager.gd
@@ -2,7 +2,8 @@ extends Node
 
 var version = "a05"
 var score: int = 0
-var balls_left: int = 5
+var balls_left: int = 1
+var gameOver: bool = false
 var canSpawn:bool = true
 var windowMode = DisplayServer.WINDOW_MODE_FULLSCREEN
 var is_paused = false
@@ -20,13 +21,14 @@ func _set_window_mode():
 func reset():
 	score = 0
 	balls_left = 5
+	gameOver = false
 	var balls_in_scene = get_node("/root/Board/Balls").get_children()
 	for b in balls_in_scene:
 		b.queue_free()
 	canSpawn = true
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("quit"):
+	if event.is_action_pressed("quit") and !gameOver:
 		is_paused = true
 		#get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	if event.is_action_pressed("restart"):
--- Game/Scripts/gui.gd
+++ Game/Scripts/gui.gd
@@ -2,16 +2,34 @@ extends Control
 
 @onready var score_text = $ScoreText
 @onready var balls_left_text = $BallsLeftText
+@onready var game_over = $GameOver
+@onready var game_over_box = $GameOver/Background
+@onready var final_score_label = $GameOver/Background/FinalScoreLabel
+@onready var animation = $AnimationPlayer
+
+var game_over_anim_done = false
 
 signal clickArea
 
 func _ready() -> void:
 	score_text.text = str(Manager.score)
 	balls_left_text.text = str(Manager.balls_left)
+	game_over.visible = false
 
 func _process(delta: float) -> void:
 	score_text.text = "Score: " + str(Manager.score)
 	balls_left_text.text = "Balls: "+str(Manager.balls_left)
+	
+	if Manager.gameOver:
+		game_over.visible = true
+		$PauseButton.visible = false
+		final_score_label.text = "Final score: "+str(Manager.score)
+		if not game_over_anim_done:
+			animation.play("GameOver")
+			game_over_anim_done = true
+		else:
+			#Wobble
+			pass
 	
 	if Manager.is_paused:
 		$PauseMenu.visible = true
@@ -37,3 +55,17 @@ func _on_pause_menu_button_pressed():
 
 func _on_clickable_area_button_up():
 	emit_signal("clickArea")
+
+# GameOver
+func _on_mainmenu_button_pressed():
+	Manager.reset()
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_again_button_pressed():
+	Manager.reset()
+	Manager.is_paused = false
+	get_tree().paused = false
+	animation.play_backwards("GameOver")
+	await get_tree().create_timer(1.45).timeout
+	animation.play("RESET")
+	get_tree().change_scene_to_file(Manager.curBoard)
